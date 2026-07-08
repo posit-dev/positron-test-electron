716370d46ce76f09e24fcf0d93916b2cf52e43bf
isabel zimmerman <isabel.zimmerman@posit.co>
2026-07-08 18:24:53 -0400

    fix: correct npm test script for Node 20 LTS and cli license headers

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,7 +1,7 @@
-/**
- * Copyright (C) 2024 Posit Software, PBC. All rights reserved.
- * License: https://github.com/posit-dev/positron-test-electron/blob/main/LICENSE
- */
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2026 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
 import { test } from 'node:test';
 import assert from 'node:assert';
 import { parseArgs } from './cli';
